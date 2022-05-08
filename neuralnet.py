d21f3b410ce03cce54b5e223bf09dd4087f0eb91
Sarah Brown <srb@ou.edu>
2022-05-08 14:17:12 -0500

    print values to txt file to confirm neurons

--- neuralnet.py
+++ neuralnet.py
@@ -67,6 +67,9 @@ def ff(n):
     return(fixed_to_float(float_to_fixed(n)))
 
 def test_build_model(first_layer_biases, first_layer_weights, second_layer_biases, second_layer_weights):
+    org_stdout = sys.stdout 
+    sys.stdout = open('neuron_layer1_confirmation.txt','w')
+
     (x_train, y_train),(x_test, y_test) = mnist.load_data() # loads mnist data
 
     num_of_testImgs = x_test.shape[0] 
@@ -78,40 +81,55 @@ def test_build_model(first_layer_biases, first_layer_weights, second_layer_biase
     correct = 0
     incorrect = 0
 
-    # iterates over each image
-    for testimg in range(1): #len(x_test)):
-        layer1 = [0.0] * 10 # creates a list with 10 entries initialized to 0.0
-        # loops over each neuron in first layer to add bias value
+    # checks pixels in test image 0
+    testimg = 0
+    layer1 = [0.0] * 10 # creates a list with 10 entries initialized to 0.0
+    # loops over each neuron in first layer to add bias value
+    for neuron in range(10): 
+        layer1[neuron] = ff(first_layer_biases[neuron])
+
+    # loops over each pixel
+    for i in range(10):
+        print(f"Bias neuron{i}:             0x"+hex(float_to_fixed(layer1[i]))[2:].zfill(8))
+    print('')
+    
+    for pixel in range(img_width*img_height):
         for neuron in range(10): 
-            layer1[neuron] = ff(first_layer_biases[neuron])
+            layer1[neuron] = ff(layer1[neuron] + ff(ff(x_test[testimg][pixel]) * ff(first_layer_weights[pixel][neuron])))
 
-        # loops over each pixel
-        for pixel in range(img_width*img_height):
-            for neuron in range(10): 
-                layer1[neuron] = ff(layer1[neuron] + ff(ff(x_test[testimg][pixel]) * ff(first_layer_weights[pixel][neuron])))
+    for i in range(10):
+        print(f"Sums neuron{i}, pixel-783:  0x"+hex(float_to_fixed(layer1[i]))[2:].zfill(8))
+    print('')
 
-        # applies ReLU
-        for neuron in range(10):
-            layer1[neuron] = max(0, layer1[neuron])
-        
-        layer2 = [0.0] * 10 # creates a list with 10 entries initialized to 0.0
-        # loops over each neuron in second layer to add bias value
-        for neuron in range(10): 
-            layer2[neuron] = ff(second_layer_biases[neuron])
+    # applies ReLU
+    for neuron in range(10):
+        layer1[neuron] = max(0, layer1[neuron])
 
-        # loops over each pixel
-        for neuron_layer1 in range(10):
-            for neuron_layer2 in range(10): 
-                layer2[neuron_layer2] = ff(layer2[neuron_layer2] + ff(ff(layer1[neuron_layer1]) * ff(second_layer_weights[neuron_layer1][neuron_layer2])))
+    for i in range(10):
+        print(f"ReLU neuron{i}, pixel-783:  0x"+hex(float_to_fixed(layer1[i]))[2:].zfill(8))
+    print('')
 
-        m = max(layer2) # applies soft max
-        print(m, layer2.index(m), y_test[testimg])
-        if (layer2.index(m) == y_test[testimg]):
-            correct += 1
-        else:
-            incorrect += 1
+    sys.stdout.close()
+    sys.stdout = org_stdout
 
-        print(f'Correct: {correct}. Incorrect: {incorrect}')
+    layer2 = [0.0] * 10 # creates a list with 10 entries initialized to 0.0
+    # loops over each neuron in second layer to add bias value
+    for neuron in range(10): 
+        layer2[neuron] = ff(second_layer_biases[neuron])
+
+    # loops over each pixel
+    for neuron_layer1 in range(10):
+        for neuron_layer2 in range(10): 
+            layer2[neuron_layer2] = ff(layer2[neuron_layer2] + ff(ff(layer1[neuron_layer1]) * ff(second_layer_weights[neuron_layer1][neuron_layer2])))
+
+    m = max(layer2) # applies soft max
+    print(m, layer2.index(m), y_test[testimg])
+    if (layer2.index(m) == y_test[testimg]):
+        correct += 1
+    else:
+        incorrect += 1
+
+    print(f'Correct: {correct}. Incorrect: {incorrect}')
 
 def mem_template(filename_mif, filename_txt, data, depth):
     org_stdout = sys.stdout
